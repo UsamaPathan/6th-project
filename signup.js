ea231015ac5291a63326a859d4df224b74c89145
UsamaPathan <usa032039@gmail.com>
2024-05-24 11:43:13 +0500

    Delete signup.js

--- signup.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const loginLink = document.querySelector('.signup-register a');
-    loginLink.addEventListener('click', function(event) {
-        event.preventDefault(); // Prevent the default action of the anchor tag
-        window.location.href = 'login.html'; // Redirect to login page
-    });
-});
